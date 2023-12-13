a1dbdd3aebdcece6634e27cc010ca89e173bddec
Kevin <millerkm@gmail.com>
2023-12-12 20:41:27 -0700

    using a 5 minute search timer... because microsoft...

--- src/quest/searchQuest.js
+++ src/quest/searchQuest.js
@@ -1,7 +1,7 @@
 class SearchQuest {
     constructor(googleTrend) {
         this._googleTrend_ = googleTrend;
-        this._searchIntervalMS = 28000;
+        this._searchIntervalMS = 300000;
         this.reset();
     }
 
